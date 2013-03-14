9f610c880055818ee9e934d787bf845ff3572d95
Alexandr Korsak <alex.korsak@gmail.com>
2013-03-14 13:59:57 +0300

    Add root view to the main

--- src/Application.js
+++ src/Application.js
@@ -1,15 +1,20 @@
-import ui.TextView as TextView;
+import device;
+import ui.StackView as StackView;
 
 exports = Class(GC.Application, function () {
 
 	this.initUI = function () {
-		var textview = new TextView({
-			superview: this.view,
-			layout: "box",
-			text: "Hello, world!",
-			color: "white"
+		var rootView = new StackView({
+			superview: this,
+			x: device.width / 2 - 160,
+			y: device.height / 2 - 240,
+			width: 720,
+			height: 1280,
+			clip: true,
+			backgroundColor: '#37B34A'
 		});
 	};
-	
+
 	this.launchUI = function () {};
 });
+
